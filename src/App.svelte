3256eb4be17b4f30e9739a0add0c5ff0f68c05cf
jona881a <jonaskunert@hotmail.com>
2022-03-14 07:51:34 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+import Tutorial from "./pages/Tutorial.svelte";
+
+</script>
+
+<main>
+	<h1>Learning Svelte Tutorial</h1>
+	<Tutorial/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/pages/Tutorial.svelte
@@ -0,0 +1,48 @@
+<script>
+	let count = 0;
+
+  $: doubled = count * 2;
+
+	function incrementCount() {
+		count += 1;
+	}
+
+  $: if (count >= 10) {
+	  alert('count is dangerously high! Resetting..');
+	  count = 0;
+}
+
+let numbers = [1, 2, 3, 4];
+
+	function addNumber() {
+		numbers.push(numbers.length + 1);
+		numbers = [...numbers, numbers.length + 1];
+	}
+
+	$: sum = numbers.reduce((t, n) => t + n, 0);
+
+</script>
+
+<p>{count} doubled is {doubled}</p>
+
+<button on:click={incrementCount}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click={addNumber}>
+	Add a number
+</button>
+
+<style>
+  button {
+    border-radius: 20px;
+  }
+
+  p {
+    font-size: 20px;
+    font-family: "Comic Sans MS";
+  }
+</style>
